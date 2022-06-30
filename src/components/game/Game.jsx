7489ee5f4494f3d823b95e3d85c0ea8d0e35409c
Bruno Patrício <bpatriciocosta@gmail.com>
2022-06-30 11:52:48 -0300

    criado lógica do gameoption e array

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,19 +1,19 @@
+import { useState } from 'react'
 import style from './Game.module.css'
 import GameOption from '../gameOption/GameOption'
 
 
 function Game () {
+
+  const [gameState, setGameState] = useState(Array(9).fill(0))
+  
   return (
     <div className={style.game}>
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
+      {
+        gameState.map((value, pos, ) =>  
+          <GameOption key={`game-option-pos-${pos}`} status={value}/>
+        )
+      }
     </div>
   )
 }
--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,9 +1,17 @@
 import style from './GameOption.module.css'
+import Icon from '../icon/Icon'
 
+const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25px"/>
 
-function GameOption () {
+function GameOption ({ status }) {
   return (
     <div className={style.gameOption}>
+      {
+        status === 1 && <GameIcon iconName="circle" />
+      }
+      {
+        status === -1 && <GameIcon iconName="x" />
+      }
       
     </div>
   )
